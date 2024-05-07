3eb3ff79c65f854b86f56f6b1c8dfa697fdfb1e2
Joshen Lim <joshenlimek@gmail.com>
2024-05-07 15:33:15 +0800

    Tiny style changes

--- apps/studio/components/interfaces/Settings/Integrations/VercelIntegration/VercelIntegrationConnectionForm.tsx
+++ apps/studio/components/interfaces/Settings/Integrations/VercelIntegration/VercelIntegrationConnectionForm.tsx
@@ -1,28 +1,22 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import toast from 'react-hot-toast'
+import * as z from 'zod'
+
+import type {
+  Integration,
+  IntegrationProjectConnection,
+} from 'data/integrations/integrations.types'
+import { useVercelConnectionUpdateMutation } from 'data/integrations/vercel-connection-update-mutate'
 import {
-  AlertDescription_Shadcn_,
-  AlertTitle_Shadcn_,
-  Alert_Shadcn_,
   FormControl_Shadcn_,
   FormDescription_Shadcn_,
   FormField_Shadcn_,
   FormItem_Shadcn_,
   FormLabel_Shadcn_,
   Form_Shadcn_,
-  IconClock,
   Switch,
-  cn,
 } from 'ui'
-import * as z from 'zod'
-
-import { ScaffoldDivider } from 'components/layouts/Scaffold'
-import type {
-  Integration,
-  IntegrationProjectConnection,
-} from 'data/integrations/integrations.types'
-import { useVercelConnectionUpdateMutation } from 'data/integrations/vercel-connection-update-mutate'
 
 const VercelIntegrationConnectionForm = ({
   connection,
@@ -49,25 +43,21 @@ const VercelIntegrationConnectionForm = ({
   })
 
   const { mutate: updateVercelConnection } = useVercelConnectionUpdateMutation({
-    onSuccess: (data) => {
-      toast.success(`Updated Supabase directory`)
-    },
+    onSuccess: () => toast.success(`Updated Supabase directory`),
   })
 
   function onSubmit(data: z.infer<typeof FormSchema>) {
+    const {
+      environmentVariablesProduction,
+      environmentVariablesPreview,
+      environmentVariablesDevelopment,
+    } = data
+
     const envSyncTargets: string[] = []
 
-    if (data.environmentVariablesProduction) {
-      envSyncTargets.push('production')
-    }
-
-    if (data.environmentVariablesPreview) {
-      envSyncTargets.push('preview')
-    }
-
-    if (data.environmentVariablesDevelopment) {
-      envSyncTargets.push('development')
-    }
+    if (environmentVariablesProduction) envSyncTargets.push('production')
+    if (environmentVariablesPreview) envSyncTargets.push('preview')
+    if (environmentVariablesDevelopment) envSyncTargets.push('development')
 
     updateVercelConnection({
       id: connection.id,
@@ -79,11 +69,11 @@ const VercelIntegrationConnectionForm = ({
   return (
     <Form_Shadcn_ {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className={'w-full space-y-6'}>
-        <div>
-          <div className="flex flex-col gap-6 px-8 py-8">
-            <h5 className="text-foreground text-sm">
-              Sync environment variables for selected target environments
-            </h5>
+        <div className="px-6 py-4 flex flex-col gap-y-4">
+          <h5 className="text-foreground text-sm">
+            Sync environment variables for selected target environments
+          </h5>
+          <div className="flex flex-col gap-4">
             <FormField_Shadcn_
               control={form.control}
               name="environmentVariablesProduction"
@@ -150,7 +140,7 @@ const VercelIntegrationConnectionForm = ({
                   <div>
                     <FormLabel_Shadcn_ className="!text">Development</FormLabel_Shadcn_>
                     <FormDescription_Shadcn_ className="text-xs text-foreground-lighter">
-                      Sync environment variables for <code>Development</code> environment.
+                      Sync environment variables for <code>development</code> environment.
                     </FormDescription_Shadcn_>
                   </div>
                 </FormItem_Shadcn_>
